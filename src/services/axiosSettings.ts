76f7e01469c68bb160da3485843dcaec159812d1
Aleksei Kosumov <AKosumovJob@gmail.com>
2023-04-30 16:11:47 +0300

    1

--- src/services/axiosSettings.ts
+++ src/services/axiosSettings.ts
@@ -3,6 +3,6 @@ import axios from 'axios'
 export const API_URL = 'https://kosnet-production.up.railway.app'
 
 export const $api = axios.create({
-	withCredentials: true,
+	withCredentials: false,
 	baseURL: API_URL,
 })
